e166faed40879c1ffca3b6f548000f7610e96948
sagar-cenation <sagarkeshri@rocketmail.com>
2017-04-09 23:57:09 +0530

    applying  K Nearest Neighbors Algorithm on realset of data of breast cancer

--- k_nearest_neighbour/k_nearest_neighbour_manually.py
+++ k_nearest_neighbour/k_nearest_neighbour_manually.py
@@ -1,22 +1,20 @@
 import numpy as np
-from math import sqrt
 import matplotlib.pyplot as plt
-import warnings #warn user when using dumb number for k
 from matplotlib import style
+import warnings # warn user when using dumb number for k
 from collections import Counter
+#dont forget this
+import pandas as pd
+import random # to shuffle the dataset
 style.use('fivethirtyeight')
 
-dataset = {'k':[[1,2],[2,3],[3,1]], 'r':[[6,5],[7,7],[8,6]]} # features
-new_features = [5,7]
 
 # for i in dataset:
 # 	for ii in dataset[i]:
 # 		plt.scatter(ii[0], ii[1], s=100, color=i)
 # same as below single line
-
 # [[plt.scatter(ii[0], ii[1], s=100, color=i) for ii in dataset[i]] for i in dataset]
-# plt.scatter(new_features[0], new_features[1], s=100)
-# plt.show()
+
 
 def k_nearest_neighbors(data, predict, k=3):
 	if len(data) >= k:
@@ -29,17 +27,44 @@ def k_nearest_neighbors(data, predict, k=3):
 			distances.append([euclidean_distance, group])
 
 	votes = [i[1] for i in sorted(distances)[:k]]
-	# print(votes)
-	# print(distances)
-	# print(Counter(votes)) ({'r': 3})
-	print(Counter(votes).most_common(1))
+	# print(Counter(votes).most_common(1))
 	vote_result = Counter(votes).most_common(1)[0][0]
-
 	return vote_result
 
-result = k_nearest_neighbors(dataset, new_features, k=3)
-print(result)
+	# votes = [i[1] for i in sorted(distances)[:k]]
+ #    vote_result = Counter(votes).most_common(1)[0][0]
+ #    return vote_result
 
-[[plt.scatter(ii[0], ii[1], s=100, color=i) for ii in dataset[i]] for i in dataset]
-plt.scatter(new_features[0], new_features[1], s=100, color=result)
-plt.show()
+# shuffle the data
+df = pd.read_csv('breast-cancer-wisconsin.data')
+df.replace('?', -99999, inplace=True)
+df.drop(['id'], 1, inplace=True)
+# print(df.head())
+full_data = df.astype(float).values.tolist() 
+# print(full_data[:5])
+random.shuffle(full_data)
+
+# slice the data
+test_size = 0.2
+train_set = {2:[],4:[]}
+test_set = {2:[],4:[]}
+train_data = full_data[:-int(test_size*len(full_data))]
+test_data = full_data[-int(test_size*len(full_data)):]
+
+for i in train_data:
+	train_set[i[-1]].append(i[:-1])
+
+for i in test_data:
+	test_set[i[-1]].append(i[:-1])
+
+correct = 0
+total = 0
+
+for group in test_set:
+	for data in test_set[group]:
+		vote = k_nearest_neighbors(train_set, data, k=5)
+		if group == vote:
+			correct += 1
+		total += 1
+
+print('Accuracy:', float(correct)/float(total))
