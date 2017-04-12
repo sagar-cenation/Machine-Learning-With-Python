a9ca97df33e923efc6b4e049712ca5a7f5a8097b
sagar-cenation <sagarkeshri@rocketmail.com>
2017-04-13 05:08:54 +0530

    compared manual algo for KNN with scikit learn KNN

--- k_nearest_neighbour/applyingKNearestNeighbour.py
+++ k_nearest_neighbour/applyingKNearestNeighbour.py
@@ -2,23 +2,28 @@ import numpy as np
 from sklearn import preprocessing, cross_validation, neighbors
 import pandas as pd
 
-df = pd.read_csv('breast-cancer-wisconsin.data')
-df.replace('?', -99999, inplace=True) #serves as outlier
-df.drop(['id'], 1, inplace=True)
+accuracies = []
+for i in range(25):
+	df = pd.read_csv('breast-cancer-wisconsin.data')
+	df.replace('?', -99999, inplace=True) #serves as outlier
+	df.drop(['id'], 1, inplace=True)
 
-X = np.array(df.drop(['class'],1))
-y = np.array(df['class'])
+	X = np.array(df.drop(['class'],1))
+	y = np.array(df['class'])
 
-X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y, test_size=0.2)
+	X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y, test_size=0.2)
 
-clf = neighbors.KNeighborsClassifier()
-clf.fit(X_train, y_train)
+	clf = neighbors.KNeighborsClassifier(n_jobs=-1)
+	clf.fit(X_train, y_train)
 
-accuracy = clf.score(X_test, y_test)
-print(accuracy)
+	accuracy = clf.score(X_test, y_test)
+	# print(accuracy)
 
-example_measures = np.array([[4,2,1,1,1,2,3,2,1],[4,2,1,2,2,2,3,2,1]]) # without id and class
-example_measures = example_measures.reshape(len(example_measures),-1) # len is used when we have to predict dynamically
+	# example_measures = np.array([[4,2,1,1,1,2,3,2,1],[4,2,1,2,2,2,3,2,1]]) # without id and class
+	# example_measures = example_measures.reshape(len(example_measures),-1) # len is used when we have to predict dynamically
 
-prediction = clf.predict(example_measures)
-print(prediction)
+	# prediction = clf.predict(example_measures)
+	# print(prediction)
+	accuracies.append(accuracy)
+
+print(sum(accuracies)/len(accuracies))
--- k_nearest_neighbour/k_nearest_neighbour_manually.py
+++ k_nearest_neighbour/k_nearest_neighbour_manually.py
@@ -29,42 +29,50 @@ def k_nearest_neighbors(data, predict, k=3):
 	votes = [i[1] for i in sorted(distances)[:k]]
 	# print(Counter(votes).most_common(1))
 	vote_result = Counter(votes).most_common(1)[0][0]
-	return vote_result
+	confidence = Counter(votes).most_common(1)[0][1] / k
+	# print(vote_result,confidence)
+	return vote_result,confidence
 
 	# votes = [i[1] for i in sorted(distances)[:k]]
  #    vote_result = Counter(votes).most_common(1)[0][0]
  #    return vote_result
 
-# shuffle the data
-df = pd.read_csv('breast-cancer-wisconsin.data')
-df.replace('?', -99999, inplace=True)
-df.drop(['id'], 1, inplace=True)
-# print(df.head())
-full_data = df.astype(float).values.tolist() 
-# print(full_data[:5])
-random.shuffle(full_data)
+accuracies = []
+for i in range(25):
+	# shuffle the data`
+	df = pd.read_csv('breast-cancer-wisconsin.data')
+	df.replace('?', -99999, inplace=True)
+	df.drop(['id'], 1, inplace=True)
+	# print(df.head())
+	full_data = df.astype(float).values.tolist() 
+	# print(full_data[:5])
+	random.shuffle(full_data)
 
-# slice the data
-test_size = 0.2
-train_set = {2:[],4:[]}
-test_set = {2:[],4:[]}
-train_data = full_data[:-int(test_size*len(full_data))]
-test_data = full_data[-int(test_size*len(full_data)):]
+	# slice the data
+	test_size = 0.4
+	train_set = {2:[],4:[]}
+	test_set = {2:[],4:[]}
+	train_data = full_data[:-int(test_size*len(full_data))]
+	test_data = full_data[-int(test_size*len(full_data)):]
 
-for i in train_data:
-	train_set[i[-1]].append(i[:-1])
+	for i in train_data:
+		train_set[i[-1]].append(i[:-1])
 
-for i in test_data:
-	test_set[i[-1]].append(i[:-1])
+	for i in test_data:
+		test_set[i[-1]].append(i[:-1])
 
-correct = 0
-total = 0
+	correct = 0
+	total = 0
 
-for group in test_set:
-	for data in test_set[group]:
-		vote = k_nearest_neighbors(train_set, data, k=5)
-		if group == vote:
-			correct += 1
-		total += 1
+	for group in test_set:
+		for data in test_set[group]:
+			vote,confidence = k_nearest_neighbors(train_set, data, k=5)
+			if group == vote:
+				correct += 1
+			# else:
+				# print(confidence)
+			total += 1
 
-print('Accuracy:', float(correct)/float(total))
+	# print('Accuracy:', float(correct)/float(total))
+	accuracies.append(correct/total)
+print(sum(accuracies)/len(accuracies))
