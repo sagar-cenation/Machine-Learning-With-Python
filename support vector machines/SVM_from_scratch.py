30a591d796685f2856ffe1c88078ae7f6917d743
sagar-cenation <sagarkeshri@rocketmail.com>
2017-04-15 08:30:09 +0530

    added svm optimization

--- support vector machines/SVM_from_scratch.py
+++ support vector machines/SVM_from_scratch.py
@@ -39,7 +39,7 @@ class Support_Vector_Machine:
 
 		# extremely expensive
 		b_range_multiple = 5
-		# 
+		# we do not need to take as small of steps with b as we do w
 		b_multiple = 5
 		latest_optimum = self.max_feature_value*10
 
@@ -48,7 +48,38 @@ class Support_Vector_Machine:
 			# we can do this because convex (no more steps to do so)
 			optimized = False
 			while not optimized:
-				pass
+				for b in np.arange(-1*(self.max_feature_value*b_range_multiple),
+								       self.max_feature_value*b_range_multiple,
+								       step*b_multiple):
+					for transformation in transforms:
+						w_t = w*transformation
+						found_option = True
+						# weakest link in the SVM fundamentally
+						# SMO attempts to fix this a bit
+						# yi(xi.w+b)>=1
+						## add a break here later
+						for i in self.data:
+							for xi in self.data[i]:
+								yi=i
+								if not yi*(np.dot(w_t,xi)+b) >=1:
+									found_option = False
+									# break
+
+						if found_option:
+							opt_dict[np.linalg.norm(w_t)] = [w_t,b] #gives mgt of the vector
+
+				if w[0] < 0:
+					optimized = True
+					print('Optimized a step.')
+				else:
+					w = w - step
+
+			norms = sorted([n for n in opt_dict])
+			# ||w|| : [w,b]
+			opt_choice = opt_dict[norms[0]]
+			self.w = opt_choice[0]
+			self.b = opt_choice[1]
+			latest_optimum = opt_choice[0][0] + step*2
 
 
 
